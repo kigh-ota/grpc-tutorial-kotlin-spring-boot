bf7a95b108fa0ceefac715e589726db08be7d6b0
Kaiichiro Ota <kigh.ota@gmail.com>
2021-09-16 01:13:07 +0900

    set up gradle

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # grpc-tutorial-kotlin-spring-boot
 
 Do https://github.com/ymmt2005/grpc-tutorial using Kotlin and Spring Boot.
+
+- Generating codes
+  - `./gradlew generateProto` -> under `build/generated/source/proto/main/`:
+    - messages in `java/`
+    - RPCs in `grpc/` and `grpckt/`
+- Implementation
+  - TODO: extend `ComputeGrpcKt.ComputeCoroutineImplBase`
+    - (cf. extend `ComputeGrpc.ComputeImplBase` for Java)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.google.protobuf.gradle.*
+
 plugins {
     kotlin("jvm") version "1.5.10"
+    id("com.google.protobuf") version "0.8.17"
 }
 
 group = "org.example"
@@ -9,6 +12,37 @@ repositories {
     mavenCentral()
 }
 
+val protobufVersion = "3.14.0"
+val grpcVersion = "1.36.0"
+val grpcKotlinVersion = "1.1.0"
+
 dependencies {
     implementation(kotlin("stdlib"))
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+
+    api("io.grpc:grpc-protobuf:$grpcVersion")
+    api("com.google.protobuf:protobuf-java-util:$protobufVersion")
+    api("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk7@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+        }
+    }
 }
